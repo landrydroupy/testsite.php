e9db17e54490ace9de2c94240ecbc05b3946eed3
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-19 09:04:27 -0500

    Update window.php

--- window.php
+++ window.php
@@ -10,7 +10,8 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h1>Post response</h1>
+	<h1>GET response</h1>
+	<h3>Using GET here shows the variables collected within the URL, this can be used to transfer more public information.</h3>
     Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
