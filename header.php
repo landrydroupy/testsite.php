d19a42bc496079b03963b19896a7f17660a72aa1
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-19 10:40:22 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+<h1>Homework 2 - MIS 4013 - Landry Droupy PHP Site</h1>
+<h3>This site will showcase the basic capabilities of PHP and show the use of GET and POST methods</h3> 
+</html>
