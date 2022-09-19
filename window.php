97088c5d013d235b1cb7939e1875698c24bb1f60
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-19 10:21:12 -0500

    Update window.php

--- window.php
+++ window.php
@@ -1,13 +1,10 @@
-<?php include("includes/index.php");?>
+
 <!DOCTYPE html>
 <html>
-<head>
-	<?php include("includes/head-tag-contents.php");?>
-</head>
+
 <body>
 
-<?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+
 
 <div class="container" id="main-content">
 	<h1>GET response</h1>
@@ -18,7 +15,7 @@ Your email address is: <?php echo $_GET["email"]; ?>
   
 </div>
 
-<?php include("includes/footer.php");?>
+
 
 </body>
 </html>
