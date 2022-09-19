29733e88ebd0694cf0932be3bdd83a0fd8a5a68e
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-18 20:43:15 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -10,7 +10,8 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h1>Post response</h1>
+	<h1>This is the POST response</h1>
+	<h3>The POST method differs from GET because it does not store the information entered in the URL</h3>
     Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
