84e0668a618b9811fead7734bd24a2ebd9cc467e
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-18 20:34:21 -0500

    Create handlepost.php

---
+++ handlepost.php
@@ -0,0 +1,23 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h1>Post response</h1>
+    Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
