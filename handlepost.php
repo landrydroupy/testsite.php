b5087a56c1d78bcdc29a7c1d063d184acd875f62
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-19 10:22:10 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,13 +1,10 @@
-<?php include("includes/a_config.php");?>
+
 <!DOCTYPE html>
 <html>
-<head>
-	<?php include("includes/head-tag-contents.php");?>
-</head>
+
 <body>
 
-<?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+
 
 <div class="container" id="main-content">
 	<h1>This is the POST response</h1>
@@ -18,7 +15,7 @@ Your email address is: <?php echo $_POST["email"]; ?>
   
 </div>
 
-<?php include("includes/footer.php");?>
+
 
 </body>
 </html>
